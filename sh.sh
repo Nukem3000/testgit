05f05d531ec2cc9606e231b369b202e19409feba
Artem <smap@artemsentyakov.ru>
2023-01-24 17:55:07 +0300

    dddd

--- sh.sh
+++ sh.sh
@@ -1,13 +1,31 @@
 #!/bin/bash
+Help()
+{
+   echo "Backup your log files and pack it to tarball."
+   echo
+   echo "Syntax: backup.sh [-u|H|v|V]"
+   echo "options:"
+   echo "u     SSH Username."
+   echo "H     Remote hostname."
+   echo "h     Print this Help."
+   echo "b     Backup dir path."
+   echo "t     Set the age of the log files in minutes."
+   echo "l     Folder with logs to backup."
+}
 
-while getopts n:c: option
+while getopts u:H:h:b:t:l option
 do 
     case "${option}"
         in
-        n)nation=${OPTARG};;
-        c)code=${OPTARG};;
+        h) # display Help
+        Help
+        exit;;
+        u)user=${OPTARG};;
+        H)hostname=${OPTARG};;
+        b)bkpdir=${OPTARG};;
+        t)t=${OPTARG};;
+        i)logpath=${OPTARG};;
     esac
 done
 
-echo "Nation : $nation"
-echo "code   : $code"
+tar -cvf $bkpdir/logs-$(date +%Y-%m-%d).tar $(find $logpath -mmin + $t)
